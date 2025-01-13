7ddef944b3e0badabebde2cefb718f8ada1a7ca9
refigo <refi@xyzcorp.io>
2025-01-13 15:16:15 +0900

    Init: Set node.js initially

---
+++ src/server.js
@@ -0,0 +1,10 @@
+import express from "express";
+
+const app = express();
+
+app.set("view engine", "pug");
+app.set("views", __dirname)
+
+
+const handleListen = () => console.log(`Listening on: http://localhost:3000`);
+app.listen(3000, handleListen);
