9e567317c0eb18b4bbf0617c26bba6791e7fccc1
refigo <refi@xyzcorp.io>
2025-01-20 16:11:12 +0900

    Feat: Set socket.io connection

--- src/server.js
+++ src/server.js
@@ -16,7 +16,13 @@ const httpServer = http.createServer(app);
 const wsServer = SocketIO(httpServer);
 
 wsServer.on("connection", (socket) => {
-    console.log(socket);
+    socket.on("enter_room", (msg, done) => {
+        console.log(msg);
+        // console.log(done);
+        setTimeout(() => {
+            done();
+        }, 10000);
+    });
 });
 
 /*
