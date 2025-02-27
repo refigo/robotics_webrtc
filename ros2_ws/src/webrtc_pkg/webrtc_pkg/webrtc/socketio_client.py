462b3e20254fba8dd7a74799142eb7098486163c
refigo <refi@xyzcorp.io>
2025-02-27 11:13:15 +0900

    Refactor: Organize socketio_client code

--- ros2_ws/src/webrtc_pkg/webrtc_pkg/webrtc/socketio_client.py
+++ ros2_ws/src/webrtc_pkg/webrtc_pkg/webrtc/socketio_client.py
@@ -10,6 +10,7 @@ from aiortc import (
     RTCIceServer,
 )
 from .media import VideoStreamTrack, AudioStreamTrack, list_media_devices
+import asyncio
 
 logger = logging.getLogger(__name__)
 
@@ -26,6 +27,11 @@ class WebRTCSocketIOClient:
         
         # Configure ICE servers
         self.ice_servers = [
+            RTCIceServer(urls="stun:stun.l.google.com:19302"),
+            RTCIceServer(urls="stun:stun1.l.google.com:19302"),
+            RTCIceServer(urls="stun:stun2.l.google.com:19302"),
+            RTCIceServer(urls="stun:stun3.l.google.com:19302"),
+            RTCIceServer(urls="stun:stun4.l.google.com:19302"),
             RTCIceServer(urls=[
                 "stun:stun.l.google.com:19302",
                 "stun:stun1.l.google.com:19302",
@@ -62,9 +68,7 @@ class WebRTCSocketIOClient:
             logger.info(f"User {user} joined the room")
             if self.pc is None:
                 await self.setup_peer_connection()
-                offer = await self.pc.createOffer()
-                await self.pc.setLocalDescription(offer)
-                await self.sio.emit("offer", ({"sdp": offer.sdp, "type": offer.type}, self.room))
+                await self.create_and_send_offer()
 
         @self.sio.on("offer")
         async def on_offer(offer_data):
@@ -74,13 +78,14 @@ class WebRTCSocketIOClient:
             await self.pc.setRemoteDescription(offer)
             answer = await self.pc.createAnswer()
             await self.pc.setLocalDescription(answer)
+            logging.info("Answer created and sent: %s", {"sdp": answer.sdp, "type": answer.type})
             await self.sio.emit("answer", ({"sdp": answer.sdp, "type": answer.type}, self.room))
+            # await self.handle_offer(offer_data)
 
         @self.sio.on("answer")
         async def on_answer(answer_data):
             if self.pc:
-                answer = RTCSessionDescription(sdp=answer_data["sdp"], type=answer_data["type"])
-                await self.pc.setRemoteDescription(answer)
+                await self.handle_answer(answer_data)
 
         @self.sio.on("ice")
         async def on_ice(ice_data):
@@ -119,18 +124,12 @@ class WebRTCSocketIOClient:
                 await self.pc.addIceCandidate(candidate)
             except Exception as e:
                 logging.error("Error processing ICE candidate: %s", e)
-            # if ice_data and self.pc:
-            #     candidate = RTCIceCandidate(
-            #         sdpMid=ice_data["sdpMid"],
-            #         sdpMLineIndex=ice_data["sdpMLineIndex"],
-            #         candidate=ice_data["candidate"],
-            #     )
-            #     await self.pc.addIceCandidate(candidate)
 
     async def setup_peer_connection(self):
         """
         Set up WebRTC peer connection
         """
+        logger.info("Setting up new peer connection")
         if self.pc:
             logger.warning("Peer connection already exists")
             return
@@ -138,7 +137,28 @@ class WebRTCSocketIOClient:
         # Create peer connection with ICE servers
         config = RTCConfiguration(iceServers=self.ice_servers)
         self.pc = RTCPeerConnection(configuration=config)
-        logger.info("Created peer connection with ICE servers: %s", self.ice_servers)
+        logger.info("Created new RTCPeerConnection")
+
+        @self.pc.on("icecandidate")
+        async def on_icecandidate(event):
+            logger.info("ICE candidate event triggered")
+            if event.candidate:
+                candidate_str = event.candidate.candidate
+                parts = candidate_str.split()
+                ip = parts[4] if len(parts) > 4 else "unknown"
+                typ = parts[parts.index("typ") + 1] if "typ" in parts else "unknown"
+                logger.info(f"ICE candidate - IP: {ip}, Type: {typ}")
+                
+                # Send to room with correct event name 'ice'
+                await self.sio.emit("ice", ({
+                    "candidate": event.candidate.candidate,
+                    "sdpMid": event.candidate.sdpMid,
+                    "sdpMLineIndex": event.candidate.sdpMLineIndex,
+                }, self.room))
+                logger.info("Sent ICE candidate to signaling server")
+            else:
+                logger.info("ICE gathering completed")
+                await self.sio.emit("ice", (None, self.room))
 
         @self.pc.on("connectionstatechange")
         async def on_connectionstatechange():
@@ -155,32 +175,61 @@ class WebRTCSocketIOClient:
         async def on_icegatheringstatechange():
             logger.info("ICE gathering state is %s", self.pc.iceGatheringState)
 
-        # Add video track
+        # Add video track first before creating offer/answer
         video_track = VideoStreamTrack(self.intermediate)
         self.pc.addTrack(video_track)
+        logger.info("Added video track to peer connection")
 
-        # Add audio track if available
-        # try:
-        #     _, audio_devices = list_media_devices()
-        #     if audio_devices:
-        #         audio_track = AudioStreamTrack(audio_devices[0]["index"])
-        #         self.pc.addTrack(audio_track)
-        #         logger.info(f"Audio track added: {audio_devices[0]['name']}")
-        # except Exception as e:
-        #     logger.warning(f"Cannot add audio track: {e}")
+    async def create_and_send_offer(self):
+        """Create and send offer"""
+        logger.info("Creating offer")
+        offer = await self.pc.createOffer()
+        logger.info("Setting local description (offer)")
+        await self.pc.setLocalDescription(offer)
+        logger.info("Offer created and set as local description")
+        
+        # Wait a bit for initial candidates to be gathered
+        while self.pc.iceGatheringState == "new":
+            await asyncio.sleep(0.1)
+        
+        await self.sio.emit("offer", ({
+            "sdp": self.pc.localDescription.sdp, # offer.sdp
+            "type": self.pc.localDescription.type # offer.type
+        }, self.room))
+        logger.info("Offer created and sent: %s", {"sdp": offer.sdp, "type": offer.type})
+        # logger.info("Offer sent to signaling server")
 
-        @self.pc.on("icecandidate")
-        async def on_icecandidate(event):
-            if event.candidate is None:
-                # ICE gathering is complete
-                await self.sio.emit("ice", (None, self.room))
-            else:
-                candidate_dict = {
-                    "candidate": event.candidate.candidate,
-                    "sdpMid": event.candidate.sdpMid,
-                    "sdpMLineIndex": event.candidate.sdpMLineIndex,
-                }
-                await self.sio.emit("ice", (candidate_dict, self.room))
+    async def handle_offer(self, offer_data):
+        """Handle incoming offer"""
+        logger.info("Handling incoming offer")
+        if self.pc is None:
+            await self.setup_peer_connection()
+            
+        offer = RTCSessionDescription(sdp=offer_data["sdp"], type=offer_data["type"])
+        logger.info("Setting remote description (offer)")
+        await self.pc.setRemoteDescription(offer)
+        
+        logger.info("Creating answer")
+        answer = await self.pc.createAnswer()
+        logger.info("Setting local description (answer)")
+        await self.pc.setLocalDescription(answer)
+        
+        # Wait a bit for initial candidates to be gathered
+        while self.pc.iceGatheringState == "new":
+            await asyncio.sleep(0.1)
+            
+        await self.sio.emit("answer", ({
+            "sdp": self.pc.localDescription.sdp,
+            "type": self.pc.localDescription.type
+        }, self.room))
+        logger.info("Answer sent to signaling server")
+
+    async def handle_answer(self, answer_data):
+        """Handle incoming answer"""
+        if self.pc:
+            answer = RTCSessionDescription(sdp=answer_data["sdp"], type=answer_data["type"])
+            logger.info("Setting remote description (answer)")
+            await self.pc.setRemoteDescription(answer)
 
     async def connect(self, server_url='http://localhost:3000'):
         """
